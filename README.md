a006606cdcf5b71a1d17511dc813f6215ac982b4
Shiny <ublue724@gmail.com>
2021-08-29 22:27:53 +0800

    Init Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Gather Mute Shortcut
+
+This chrome extension allows you to use shortcuts to controlling the mic on/off
+
+| shortcut        | behavior                     |
+|-----------------|------------------------------|
+| Ctrl/Cmd + D    | Toggle Mic                   |
+| Space           | Un-mute while pressing space |
+
+![](https://www.youtube.com/watch?v=a1TFK_PiUdg)
---
+++ content-script.js
@@ -0,0 +1,49 @@
+;(() => {
+  const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+  const getIsMute = () => {
+    const key = `${window.location.pathname.split('/').slice(2).join("\\")}/audioMuteClicked`
+    return localStorage.getItem(key) === 'true'
+  }
+  const getAudioButton = () => {
+    return document.querySelector('.GameVideo-self-video-container button')
+      ||  document.querySelector('.GameCanvasContainer-main button')
+  }
+
+  window.addEventListener('keydown', (e) => {
+    if (e.target.tagName === 'INPUT') {
+      return
+    }
+    const audioBtn = getAudioButton()
+    const isMute = getIsMute();
+    if (!e.repeat) {
+      if (e.key === ' ') {
+        e.stopPropagation()
+        e.preventDefault();
+        if (isMute) {
+          audioBtn.click()
+        }
+      }
+    }
+
+    if (e.key.toUpperCase() === 'D' && isMac ? e.metaKey : e.ctrlKey) {
+      e.stopPropagation()
+      e.preventDefault();
+      audioBtn.click()
+    }
+  }, {capture: true})
+
+  window.addEventListener('keyup', (e) => {
+    if (e.target.tagName === 'INPUT') {
+      return
+    }
+    if (e.key === ' ') {
+      e.stopPropagation()
+      e.preventDefault();
+      const audioBtn = getAudioButton()
+      const isMute = getIsMute();
+      if (!isMute) {
+        audioBtn.click()
+      }
+    }
+  }, {capture: true})
+})();
